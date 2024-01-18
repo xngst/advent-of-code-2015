e8a9ca75313b1c22c7d15071fa8ee9678f5e6780
Xngst <tarcvzf@protonmail.ch>
2024-01-18 21:50:59 +0100

    add d6/2

--- d6/d6.py
+++ d6/d6.py
@@ -27,6 +27,35 @@ for instruction in data:
             else:
                 grid[x][y] = 1 - grid[x][y]
 
-
 r1 = sum(map(sum, grid))
-print(f"R1:{r1}")
+print(f"R1: {r1}")
+
+grid = [[0 for x in range(1000)] for y in range(1000)]
+
+for instruction in data:
+    parts = instruction.split()
+
+    if "on" in instruction or "off" in instruction:
+        action = parts[1]
+        start_x, start_y = map(int, parts[2].split(","))
+        end_x, end_y = map(int, parts[4].split(","))
+
+    if "toggle" in instruction:
+        action = parts[0]
+        start_x, start_y = map(int, parts[1].split(","))
+        end_x, end_y = map(int, parts[3].split(","))
+
+    for x in range(start_x, end_x + 1):
+        for y in range(start_y, end_y + 1):
+            if action == "on":
+                grid[x][y] = grid[x][y] + 1
+            elif action == "off":
+                if grid[x][y] <= 0:
+                    pass
+                else:
+                    grid[x][y] = grid[x][y] - 1
+            elif action == "toggle":
+                grid[x][y] = grid[x][y] + 2
+
+r2 = sum(map(sum, grid))
+print(f"R2: {r2}")
