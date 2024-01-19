0294bc882c7b904dbc81d25e835f75b0a93d078b
Xngst <tarcvzf@protonmail.ch>
2024-01-19 18:31:42 +0100

    refactor d6.py

--- d6/d6.py
+++ d6/d6.py
@@ -1,13 +1,13 @@
 with open("input.txt", "r") as f:
     data = f.readlines()
 
-#PART1
+# PART1
 
 grid = [[0 for x in range(1000)] for y in range(1000)]
 
-for instruction in data:
-    parts = instruction.split()
 
+def get_coord(instruction: str):
+    parts = instruction.split()
     if "on" in instruction or "off" in instruction:
         action = parts[1]
         start_x, start_y = map(int, parts[2].split(","))
@@ -17,6 +17,12 @@ for instruction in data:
         action = parts[0]
         start_x, start_y = map(int, parts[1].split(","))
         end_x, end_y = map(int, parts[3].split(","))
+
+    return action, start_x, start_y, end_x, end_y
+
+
+for instruction in data:
+    action, start_x, start_y, end_x, end_y = get_coord(instruction)
 
     for x in range(start_x, end_x + 1):
         for y in range(start_y, end_y + 1):
@@ -30,22 +36,12 @@ for instruction in data:
 r1 = sum(map(sum, grid))
 print(f"R1: {r1}")
 
-#PART2
+# PART2
 
 grid = [[0 for x in range(1000)] for y in range(1000)]
 
 for instruction in data:
-    parts = instruction.split()
-
-    if "on" in instruction or "off" in instruction:
-        action = parts[1]
-        start_x, start_y = map(int, parts[2].split(","))
-        end_x, end_y = map(int, parts[4].split(","))
-
-    if "toggle" in instruction:
-        action = parts[0]
-        start_x, start_y = map(int, parts[1].split(","))
-        end_x, end_y = map(int, parts[3].split(","))
+    action, start_x, start_y, end_x, end_y = get_coord(instruction)
 
     for x in range(start_x, end_x + 1):
         for y in range(start_y, end_y + 1):
